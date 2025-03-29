0ce79724a0950ca3d363333c8913ff3a3041f847
SandyVictor-cs <sandyvictor124@gmail.com>
2025-03-29 14:57:07 +0200

    typograph done

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Typograph from "./Components/Atoms/Typograph";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+  return (
+    <div className="container mt-5">
+      <Typograph variant="h1" color="black" bold>
+        عنوان رئيسي H1 باللون الأسود
+      </Typograph>
+      <Typograph variant="h2" color="dark">
+        عنوان فرعي H2 بالرمادي الغامق
+      </Typograph>
+      <Typograph variant="h3" color="medium" bold>
+        عنوان صغير H3 بالرمادي المتوسط
+      </Typograph>
+      <Typograph variant="p" color="light">
+        فقرة نصية عادية بالرمادي الفاتح
+      </Typograph>
+      <Typograph variant="blockquote" color="dark" bold>
+        اقتباس بلون رمادي غامق
+      </Typograph>
+    </div>
+  );
+};
+
+export default App;
