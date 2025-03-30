27e93ed82b05c1770ff603738ea15f190a4e588e
shahdessamm <116279418+shahdessamm@users.noreply.github.com>
2025-03-30 05:46:42 +0200

    button component and its style

--- FrontEnd/src/Components/Atoms/Button.jsx
+++ FrontEnd/src/Components/Atoms/Button.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import PropTypes from "prop-types";
+import "../Styles/atoms.css";
+import Typography from "./Typograph";
 
-export default function Button() {
-  return <div>
-    
-  </div>;
-}
+const Button = ({ children, color = "blue", shape = "default", size = "small", icon, iconSize = 24, iconPosition = "left", onClick, disabled }) => {
+  return (
+    <button
+      className={`button ${color} ${shape} ${size}`}  
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {icon && iconPosition === "left" && (
+        <span className="icon" style={{ fontSize: `${iconSize}px` }}>{icon}</span>
+      )}
+      <Typography>{children}</Typography> 
+      {icon && iconPosition === "right" && (
+        <span className="icon" style={{ fontSize: `${iconSize}px` }}>{icon}</span>
+      )}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  children: PropTypes.node,
+  color: PropTypes.oneOf(["blue", "trans", "white","black"]),
+  shape: PropTypes.oneOf(["default", "small-circle","large-circle"]),
+  size: PropTypes.oneOf(["small", "default", "large"]),
+  icon: PropTypes.node,
+  iconSize: PropTypes.number, 
+  iconPosition: PropTypes.oneOf(["left", "right"]), 
+  onClick: PropTypes.func,
+  disabled: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  color: "blue",
+  shape: "default",
+  size: "small",
+  iconSize: 24, 
+  iconPosition: "left",
+  onClick: () => {},
+  disabled: false,
+  children: null,
+};
+
+export default Button;
