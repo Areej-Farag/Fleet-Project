cca31a89d562d0b9ee66ce59868c9b4865168738
Areej-Farag <rerefarag60@gmail.com>
2025-04-03 19:34:56 +0200

    Updates in atoms

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,12 +1,12 @@
 
 import React from 'react';
-import Rate from './Components/Atoms/Rate';
+
 
 function App() {
     return (
-        <div className="App">
-            <Rate rating="4.9" reviewCount="123" />
-        </div>
+      <>
+      
+      </>
     );
 }
 
