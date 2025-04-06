614535a0d1c98f44e757ab68564be8824f596a8f
Areej-Farag <rerefarag60@gmail.com>
2025-04-07 01:00:06 +0200

    Sign Up form designed with responsive

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 
 import PaymentTemplate from "./Components/Templates/PaymentTemplate";
 
-import BookingConfirmationTemplate from "./Components/Templates/BookingConfirmationTemplate";
+import SignUpForm from "./Components/Organisms/SignUpForm";
 
 function App() {
   return (
 
     <>
-    <PaymentTemplate/>
+    <SignUpForm />
     </>
   );
 }
