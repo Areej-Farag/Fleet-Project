77f4c38d6a8dfbd64a161a198237b171d0d5a9bd
sherouk afifi <sheroukafifinasr@gmail.com>
2025-04-07 22:19:10 +0200

    edits on molecules

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
+import Navbar from "./Components/Molecules/Navbar";
+import Footer from "./Components/Molecules/Footer";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "bootstrap-icons/font/bootstrap-icons.css";
 
-const App = () => {
-  return( <>
-  
-  </>);
-};
+function App() {
+  return (
+    <div className="app-container">
+      <Navbar />
+      <main className="content container py-4">
+        {/* هنا ممكن تضيف مكوناتك الرئيسية للصفحة */}
+        
+      </main>
+      <Footer />
+    </div>
+  );
+}
 
 export default App;
