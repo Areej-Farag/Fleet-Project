84b4087aac5125e92fc44ebf5110cdb77d292463
Areej-Farag <rerefarag60@gmail.com>
2025-04-08 22:46:33 +0200

    Changes

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 
-import PaymentTemplate from "./Components/Templates/PaymentTemplate";
-
+import PaymentTemplate  from "./Components/Templates/PaymentTemplate";
+import BookingConfirmationTemplate from "./Components/Templates/BookingConfirmationTemplate";
 import SignUpForm from "./Components/Organisms/SignUpForm";
 
 function App() {
   return (
 
     <>
-    <SignUpForm />
+    <PaymentTemplate/>
+    <BookingConfirmationTemplate />
     </>
   );
 }
