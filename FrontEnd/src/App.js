9100609e6f05c94ff15e573512712a11a9cbbbb7
Areej-Farag <rerefarag60@gmail.com>
2025-04-09 15:45:36 +0200

    SecurityCode UI and responsive

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,16 +1,10 @@
 import React from "react";
 
-import PaymentTemplate  from "./Components/Templates/PaymentTemplate";
-import BookingConfirmationTemplate from "./Components/Templates/BookingConfirmationTemplate";
-import SignUpForm from "./Components/Organisms/SignUpForm";
-import DigitedInput from "./Components/Atoms/DigitedInput";
 function App() {
   return (
 
     <>
-    <DigitedInput />
-    <PaymentTemplate/>
-    <BookingConfirmationTemplate />
+
     </>
   );
 }
