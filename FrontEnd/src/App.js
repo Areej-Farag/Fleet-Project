5b364a4cbcb9084dbb9a863e0e4c2e3c730bcae3
Areej-Farag <rerefarag60@gmail.com>
2025-04-09 16:55:02 +0200

    UserConfirmation UI and responsive

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
-
+import UserConfirmation from "./Components/Organisms/UserConfirmation";
+import SignUpForm from "./Components/Organisms/SignUpForm";
+import SignInForm from "./Components/Organisms/SignInForm";
+import SecurityCode from "./Components/Organisms/SecurityCode";
 function App() {
   return (
 
     <>
-
+    <SecurityCode/>
     </>
   );
 }
