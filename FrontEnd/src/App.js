c0f57062aaf8718559822a5261ab461da0529c62
Areej-Farag <rerefarag60@gmail.com>
2025-04-10 22:14:03 +0200

    hostcard organism added with responsive

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Comment from "./Components/Molecules/Comment";
 import { trips } from "./Mocks/Trips";
 import TripComments from "./Components/Organisms/TripComments";
+import HostCard from "./Components/Organisms/HostCard";
 function App() {
   return (
     <>
-      <TripComments />
+      {/* <TripComments /> */}
 
+    <HostCard />
     </>
   );
 }
