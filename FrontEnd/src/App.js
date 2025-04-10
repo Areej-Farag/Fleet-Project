715b036795bf0e21e82eba288d4cd1b2862b6a8b
Areej-Farag <rerefarag60@gmail.com>
2025-04-10 17:04:56 +0200

    Comments holder added in origanisms

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
-
+import Comment from "./Components/Molecules/Comment";
+import { trips } from "./Mocks/Trips";
+import TripComments from "./Components/Organisms/TripComments";
 function App() {
   return (
-
     <>
+      <TripComments />
 
     </>
   );
 }
 
 export default App;
-
