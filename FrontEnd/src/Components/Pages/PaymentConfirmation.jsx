7abf5d84343b456de37fc3d6ef2803ccc8d02cbd
Areej-Farag <rerefarag60@gmail.com>
2025-04-11 17:48:59 +0200

    Pages jsx added with routers

---
+++ FrontEnd/src/Components/Pages/PaymentConfirmation.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PaymentConfirmation() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -4,21 +4,32 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
-import { BrowserRouter } from 'react-router-dom';
-
+import Trip from "./Components/Pages/Trip";
+import PaymentConfirmation from "./Components/Pages/PaymentConfirmation";
+import Payment from "./Components/Pages/Payment";
+import Governate from "./Components/Pages/Governate";
+import Home from "./Components/Pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  
   <BrowserRouter>
     <App />
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="/Trip/:tripId" element={<Trip />} />
+      <Route
+        path="/PaymentConfirmation/:bookingId"
+        element={<PaymentConfirmation />}
+      />
+      <Route path="/Payment/:bookingId" element={<Payment />} />
+      <Route path="/Governate/:governateId" element={<Governate />} />
+      <Route path="*" element={<h1>404 Page Not Found</h1>} />
+    </Routes>
   </BrowserRouter>
-  
-  );
-  
+);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-
