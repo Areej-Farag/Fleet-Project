0d2bbc62ed03c61ca493ecccab1de4e863c30c39
shahdessamm <116279418+shahdessamm@users.noreply.github.com>
2025-04-14 09:43:13 +0200

    page

--- FrontEnd/src/Components/Pages/Governate.jsx
+++ FrontEnd/src/Components/Pages/Governate.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import TripTemplate from "../Templates/CardTemp";
+import NearbyGovernoratesSectionTemplate from "../Templates/NearbyGovernoratesSectionTemplate";
+import { governates } from "../../Mocks/Governates";
+import CityOverview from "../Templates/CityOverview"
 
-export default function Governate() {
+const TripsPage = () => {
   return (
-    <div>
-      
+    <div className="trips-page">
+      <TripTemplate />
+      <NearbyGovernoratesSectionTemplate
+        governorates={governates}
+        sectionTitle="Expolre Trips In Egypt"
+      />
+      <CityOverview description="“Explore Northland’s stunning beaches, lush forests, and rich Māori heritage. Sail through the Bay of Islands, visit ancient Kauri trees, and soak in endless adventures. Experience the magic of New Zealand’s Winterless North!”" videoUrl="https://youtu.be/au8hr-0c92I?si=aBbrDfO5WEKFJgtZ"/>
     </div>
-  )
-}
+  );
+};
+
+export default TripsPage;
