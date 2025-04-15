0ae095758c0dc00df1d39481cc45098ec76e83fe
shahdessamm <116279418+shahdessamm@users.noreply.github.com>
2025-04-15 23:30:34 +0200

    pages

--- FrontEnd/src/Components/Pages/Trip.jsx
+++ FrontEnd/src/Components/Pages/Trip.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
 
-export default function Trip() {
+import React from "react";
+import TripDetailsTemplate from "../Templates/TripDetailsTemplate"; 
+import { trips } from "../../Mocks/Trips";
+import { useParams } from "react-router-dom";
+import NearbyGovernoratesSectionTemplate from "../Templates/RandomGovern-Card";
+import { cities as governates } from '../../Mocks/Governates'
+import TripCommentsSection from "../Templates/TripCommentsSec"
+const TripPage = () => {
+  const { tripId } = useParams();
+  const trip = trips.find((t) => t.id === tripId);
+
+  if (!trip) {
+    return <div>Trip not found</div>;
+  }
+
   return (
     <div>
-      <h1>Trip</h1>
+      <TripDetailsTemplate trip={trip} />
+      <NearbyGovernoratesSectionTemplate
+            governorates={governates}
+            sectionTitle="Explore Trips In Egypt"
+          />
+          <TripCommentsSection/>
     </div>
-  )
-}
+
+  );
+};
+
+export default TripPage;
