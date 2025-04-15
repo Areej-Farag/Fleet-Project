59de6fe20dc50b103bd971ea95c8694c925ee801
Areej-Farag <rerefarag60@gmail.com>
2025-04-15 23:48:26 +0200

    Fixing bug

--- FrontEnd/src/Components/Pages/Home.jsx
+++ FrontEnd/src/Components/Pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import GovernatesPage from '../Templates/choosegover'
 export default function Home() {
   return (
     <div>
-      
+      <GovernatesPage/>
     </div>
   )
 }
