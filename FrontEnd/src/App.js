8aa978c0b98a4d177427a5acebb97593c0f7dcd3
Areej-Farag <rerefarag60@gmail.com>
2025-04-15 13:24:44 +0200

    trip payment detailes organism

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -4,12 +4,13 @@ import Comment from "./Components/Molecules/Comment";
 import { trips } from "./Mocks/Trips";
 import TripComments from "./Components/Organisms/TripComments";
 import HostCard from "./Components/Organisms/HostCard";
+import TripPaymentDetailes from "./Components/Organisms/TripPaymentDetailes";
 
 function App() {
   return (
     <>
       <Navbar />
-      {/* <TripComments /> */}
+          <TripPaymentDetailes />
       <HostCard />
     </>
   );
