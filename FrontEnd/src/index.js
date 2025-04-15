1332b44ad5d2352c8a5fc27947607d5edcbf809a
shahdessamm <116279418+shahdessamm@users.noreply.github.com>
2025-04-15 23:43:28 +0200

    index

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -3,21 +3,39 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import Navbar from "./Components/Organisms/Navbar";
-import Footer from "./Components/Organisms/Footer";
+
+import TripsPage from "./Components/Pages/Governate";
+import Trip from "./Components/Pages/Trip";
+import PaymentConfirmation from "./Components/Pages/PaymentConfirmation";
+import Payment from "./Components/Pages/Payment";
+import Governate from "./Components/Pages/Governate";
+import GovernatesPage from "./Components/Templates/choosegover";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <Navbar />
-    <App />
-    <Footer />
+    <Routes>
+      <Route path="/" element={<App />}>
+      
+        <Route index element={<Navigate to="/governates" />} />
+
+        
+        <Route path="governates" element={<GovernatesPage />} />
+        <Route path="trips/:governateId" element={<TripsPage />} />
+        <Route path="trip/:tripId" element={<Trip />} />
+        <Route path="paymentConfirmation/:bookingId" element={<PaymentConfirmation />} />
+        <Route path="payment/:bookingId" element={<Payment />} />
+        <Route path="governate/:governateId" element={<Governate />} />
+
+  
+        <Route path="*" element={<h1>404 Page Not Found</h1>} />
+      </Route>
+    </Routes>
   </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
