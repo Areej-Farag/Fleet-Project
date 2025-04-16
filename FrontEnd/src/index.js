3373f25e67b5617978a4c0a94c08d606e1fa2af6
sherouk afifi <sheroukafifinasr@gmail.com>
2025-04-16 17:56:34 +0200

    added herosection tp home

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -5,8 +5,9 @@ import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+
 import App from "./App";
-import Home from "./Components/Pages/Home";
+
 import TripsPage from "./Components/Pages/Governate";
 import Trip from "./Components/Pages/Trip";
 import PaymentConfirmation from "./Components/Pages/PaymentConfirmation";
@@ -19,8 +20,9 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
-        {/* هنا هنخلي الصفحة الرئيسية تعرض Home */}
-        <Route index element={<Home />} /> {/* هنا صفحة الهوم */}
+      
+        <Route index element={<Navigate to="/governates" />} />
+
         
         <Route path="governates" element={<GovernatesPage />} />
         <Route path="trips/:governateId" element={<TripsPage />} />
@@ -29,7 +31,7 @@ root.render(
         <Route path="payment/:bookingId" element={<Payment />} />
         <Route path="governate/:governateId" element={<Governate />} />
 
-        {/* صفحة 404 */}
+  
         <Route path="*" element={<h1>404 Page Not Found</h1>} />
       </Route>
     </Routes>
