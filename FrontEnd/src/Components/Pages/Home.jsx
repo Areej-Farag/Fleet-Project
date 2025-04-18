780f53706d27b266c2270642b7ca08606f54988b
sherouk afifi <sheroukafifinasr@gmail.com>
2025-04-18 14:10:57 +0200

    change shape of search bar

--- FrontEnd/src/Components/Pages/Home.jsx
+++ FrontEnd/src/Components/Pages/Home.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import HeroSection from '../Organisms/Herosection'
-import GovernatesPage from '../Templates/choosegover'
-
-
-
+import React from "react";
+import HeroSection from "../Organisms/Herosection";
+import GovernatesPage from "../Templates/choosegover";
+import NearbyGovernoratesSectionTemplate from "../Templates/RandomGovern-Card";
+import { cities as governates } from '../../Mocks/Governates'
+import AirSleepDreamSection from "./Components/Organisms/AirSleepDreamSection";
 export default function Home() {
   return (
-    <div>
-    <GovernatesPage/>
-
-    <HeroSection />
-    </div>
-  )
+    <>
+      <div>
+        <GovernatesPage />
+      </div>
+      <div> <NearbyGovernoratesSectionTemplate
+        governorates={governates}
+        sectionTitle="Explore Trips In Egypt"
+      /></div>
+      <div>
+        <HeroSection />
+        <AirSleepDreamSection />
+      </div>
+    </>
+  );
 }
