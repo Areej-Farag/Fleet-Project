ad065aaf4a69e06aa389f3d3733b2e11004aeb47
SandyVictor-cs <sandyvictor124@gmail.com>
2025-04-19 18:40:42 +0200

    wishlist page

--- FrontEnd/src/App2.js
+++ FrontEnd/src/App2.js
@@ -1,19 +1,15 @@
 import React from "react";
-import ImageGridTemplate from "./Components/Templates/ImageGridTemplate";
-import img1 from "./assets/Images/img1.PNG";
-import img2 from "./assets/Images/img2.PNG";
-import img3 from "./assets/Images/img3.PNG";
-import primary from "./assets/Images/primary img.png";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import WishlistPage from "./Components/Pages/Wishlist.jsx";
 
-const App2 = () => {
-  const mainImage = [primary];
-  const smallImages = [img1, img2, img3];
-
+function App() {
   return (
-    <div className="App">
-      <ImageGridTemplate mainImage={mainImage} smallImages={smallImages} />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<WishlistPage />} />
+      </Routes>
+    </Router>
   );
-};
+}
 
-export default App2;
+export default App;
