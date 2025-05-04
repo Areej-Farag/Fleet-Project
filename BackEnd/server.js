2c65705b96460766144b00a71adbc5b1992ea2de
Areej-Farag <rerefarag60@gmail.com>
2025-05-05 00:18:04 +0300

    finishing Trip backend

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,12 +1,24 @@
+const connectToDatabase = require('./config/db');
 const express = require('express');
 const app = express();
-require('dotenv').config();
 app.use(express.json());
+require('dotenv').config();
 const cors = require('cors');
 app.use(cors());
-const routes = require('./routes/routes');
-app.use('/', routes);
 
+// Connect To Database
+connectToDatabase();
+
+// Routes
+const tripsRoute = require('./routes/trips.route');
+
+app.use('/trips', tripsRoute);
+
+// Connecting The Frontend With The Backend
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  }));
 
 const handleError = (err, req, res, next) => {
     console.log(err.message);
@@ -15,5 +27,5 @@ const handleError = (err, req, res, next) => {
 app.use(handleError);
 
 app.listen(process.env.PORT , () => {
-    console.log('Server is running on port 3000' , `http://localhost:${process.env.PORT}`);
+    console.log('Server is running on port 4000' , `http://localhost:${process.env.PORT}`);
 });
