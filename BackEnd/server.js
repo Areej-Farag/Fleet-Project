51b9603f4e9d5d36820406c5e4a805fadd2fad66
SandyVictor-cs <sandyvictor124@gmail.com>
2025-05-06 17:20:19 +0300

    reviwes controller and route

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,31 +1,39 @@
-const connectToDatabase = require('./config/db');
-const express = require('express');
+const connectToDatabase = require("./config/db");
+const express = require("express");
 const app = express();
+const reviewRoutes = require("./routes/review.route");
 app.use(express.json());
-require('dotenv').config();
-const cors = require('cors');
+require("dotenv").config();
+const cors = require("cors");
 app.use(cors());
 
 // Connect To Database
 connectToDatabase();
 
 // Routes
-const tripsRoute = require('./routes/trips.route');
+const tripsRoute = require("./routes/trips.route");
 
-app.use('/trips', tripsRoute);
+app.use("/trips", tripsRoute);
+
+app.use("/api", reviewRoutes);
 
 // Connecting The Frontend With The Backend
-app.use(cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  }));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  })
+);
 
 const handleError = (err, req, res, next) => {
-    console.log(err.message);
-    res.status(500).send({ error: err.message });
-}
+  console.log(err.message);
+  res.status(500).send({ error: err.message });
+};
 app.use(handleError);
 
-app.listen(process.env.PORT , () => {
-    console.log('Server is running on port 4000' , `http://localhost:${process.env.PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(
+    "Server is running on port 4000",
+    `http://localhost:${process.env.PORT}`
+  );
 });
